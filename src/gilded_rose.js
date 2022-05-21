ca2a1eded13d95afb456898dfdd4b25ab8476a0e
Antti Ollikkala <antti@huom.io>
2022-05-21 23:12:26 +0300

    simplified

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -21,11 +21,10 @@ class Shop {
                     if (item.sellIn < 0 && item.quality < 50) item.quality++;
                     break
                 case "Backstage passes to a TAFKAL80ETC concert":
-                    item.sellIn++
-                    if (item.quality < 50) item.quality++
-                    if (item.sellIn <= 10 && item.quality < 50) item.quality++;
-                    if (item.sellIn <= 5 && item.quality < 50) item.quality++;
-                    item.sellIn--;
+                    item.quality++
+                    if (item.sellIn < 10) item.quality++;
+                    if (item.sellIn < 5) item.quality++;
+                    item.quality = Math.min(50, item.quality)
                     if (item.sellIn < 0) item.quality = 0;
                     break
                 case "Sulfuras, Hand of Ragnaros":
